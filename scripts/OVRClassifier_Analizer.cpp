ae8172ace1172a37445bf91b90cc1acd6b83e797
Ubuntu <ubuntu@ip-172-31-31-24.ec2.internal>
2020-05-17 02:41:48 +0000

    Fixing a conflict

--- scripts/OVRClassifier_Analizer.cpp
+++ scripts/OVRClassifier_Analizer.cpp
@@ -11,8 +11,8 @@ int main(){
     // Reading files
     time_t t1, t2;
     time(&t1);
-    Dataset train("../data/representation.train.csv", "../data/true_labels.train.csv", NER);
-    Dataset test("../data/representation.testa.csv", "../data/true_labels.testa.csv", NER);
+    Dataset train("train", NER);
+    Dataset test("testa", NER);
     time(&t2);
     std::cout << "Duration of file read: " << t2-t1 << "s\n";
 
